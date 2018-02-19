027e55eea440d815cd7763a0fef43da41a80a7a6
JackFriedberg <JackFriedberg@gmail.com>
2018-02-18 19:24:25 -0600

    read/print

--- PA2-18a-TemplateCode/PA2.cpp
+++ PA2-18a-TemplateCode/PA2.cpp
@@ -26,51 +26,14 @@ int main()
     descen10000.csv ascen10000.csv	rand10000.csv
   */
 
-
-  /*
-  std::ifstream in("descen10.csv");
-  std::string file;
-  std::string temp;
-  */
-
   std::string fileName = "descen10.csv";
   std::vector<Flight> flights  = readFlights(fileName);
   printFlights(flights);
 
 
+  // APPLYING SORTS
 
-  //while(in.get(temp)){
-    //file += temp;
-    //std::cout << file;
-  //}
-
-  //std::cout << file << std::endl;
-
-  //std::vector<Flight> flights = readFlights(file);
-  //std::cout << flights.at(0).destination << std::endl;
-  //printFlights(flights);
-
-  /*
-    then use each of the sorting functions on each of the generated vectors
-  */
-
-  /*
-    then output each of the resultant sorted vectors
-    format them so that they look like the table of the front of the 
-    instructions
-
-    Flight Number	Destination Departure   Time	        Gate Number
-    AA223			LAS VEGAS	21:15			A3
-    BA023			DALLAS		21:00			A3
-    AA220			LONDON		20:30			B4
-    VI303			MEXICO		19:00			A7
-    BA087			LONDON		17:45			A7
-    AA342			PARIS		16:00			A7
-    VI309			PRAGUE		13:20			F2
-    QU607			TORONTO		08:30			F2
-    AA224			SYDNEY		08:20			A7
-    AF342			WASHINGTON	07:45			A3
-  */
+  // NEED TO PRINT TO FILE
 
   /*
     then fill out the rest of the questions on the instructions 
@@ -79,20 +42,18 @@ int main()
     - to test your functions experimentally use difftime explained here 
     http://www.cplusplus.com/reference/ctime/time/
   */
-
   return 0;
 }
 
-//read in the flights from the input file at fileName and store them in a vector
-std::vector<Flight> readFlights(std::string file){
+//read in flights from input file at fileName and store in a vector
+std::vector<Flight> readFlights(std::string fileName){
   std::vector<Flight> flights;
-  std::ifstream inFile(file.c_str());
+  std::ifstream inFile(fileName.c_str());
   std::string dummy;  
   getline(inFile, dummy);
   while(!inFile.eof()) {
     Flight temp;
     std::string str;
-
     getline(inFile, str, ',');
     temp.flightNum = str;
     getline(inFile, str, ',');
@@ -101,16 +62,18 @@ std::vector<Flight> readFlights(std::string file){
     temp.departureTime = str;
     getline(inFile, str, '\n');
     temp.gateNum = str;
-
     flights.push_back(temp);
   }
   return flights;
 }
 
 void printFlights(std::vector<Flight> flights){
-  std::cout << flights.size() << std::endl;
+  printf("%-15s %-20s %-15s %-s\n", "Flight Number", 
+                                  "Destination", 
+                                  "Departure Time",
+                                  "Gate Number");
   for(int i = 0; i < flights.size(); i++){
-    printf("%7s %20s %7s %7s \n", flights.at(i).flightNum.c_str(), 
+    printf("%-15s %-20s %-15s %-s\n", flights.at(i).flightNum.c_str(), 
                                   flights.at(i).destination.c_str(), 
                                   flights.at(i).departureTime.c_str(),
                                   flights.at(i).gateNum.c_str());
