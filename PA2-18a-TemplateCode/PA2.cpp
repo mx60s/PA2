51de8261fdea8789f72c5769e85260b0fbf09a04
rstadpatri <rstadpatri@gmail.com>
2018-02-20 20:22:11 -0600

    fixed selection sort
    maybe

--- PA2-18a-TemplateCode/PA2.cpp
+++ PA2-18a-TemplateCode/PA2.cpp
@@ -1,8 +1,8 @@
 /*
-	Maggie von Ebers, Jack Friedberg, Reuben Tadpatri
-	CSCE 221-505
-	Programming Assignment 2
-	PA2.cpp
+Maggie von Ebers, Jack Friedberg, Reuben Tadpatri
+CSCE 221-505
+Programming Assignment 2
+PA2.cpp
 */
 
 
@@ -12,100 +12,101 @@
 #include <stdlib.h>
 
 #include "sort.h"
-
-std::vector<Flight> readFlights(std::string fileName);
-void printFlights(std::vector<Flight> flights);
+using namespace std;
+vector<Flight> readFlights(string fileName);
+void printFlights(vector<Flight> flights);
 
 int main()
 {
-  /*
-    first read flights in files using readFlights()
-    descen10.csv	ascen10.csv     rand10.csv
-    descen100.csv	ascen100.csv	rand100.csv
-    descen1000.csv	ascen1000.csv	rand1000.csv
-    descen10000.csv ascen10000.csv	rand10000.csv
-  */
+	/*
+	first read flights in files using readFlights()
+	descen10.csv	ascen10.csv     rand10.csv
+	descen100.csv	ascen100.csv	rand100.csv
+	descen1000.csv	ascen1000.csv	rand1000.csv
+	descen10000.csv ascen10000.csv	rand10000.csv
+	*/
 
-  // APPLYING SORTS
-    // Insertion sort (working)
-    std::string fileName = "rand10.csv";
-    std::vector<Flight> flights  = readFlights(fileName);
-    printFlights(flights);
-
-    flights = insertion_sort(flights, ByDestination);
-    std::cout << std::endl << "BY DESTINATION:" << std::endl << std::endl;
-    printFlights(flights);
-
-    flights  = readFlights(fileName);
-        
-    flights = insertion_sort(flights, ByDepartureTime);
-    std::cout << std::endl << "BY TIME:" << std::endl << std::endl;
-    printFlights(flights);
+	// APPLYING SORTS
+	// Insertion sort (working)
+	string fileName = "rand10.csv";
+	vector<Flight> flights = readFlights(fileName);
+	printFlights(flights);
+	flights = selection_sort(flights, ByDestination);
+	cout << endl << "BY DESTINATION:" << endl << endl;
+	printFlights(flights);
+	flights = selection_sort(flights, ByDepartureTime);
+	cout << endl << "BY TIME:" << endl << endl;
+	printFlights(flights);
 
 
-  /*
-    then fill out the rest of the questions on the instructions 
-    - the number of comparisons
+	/*
+	then fill out the rest of the questions on the instructions
+	- the number of comparisons
 
-    - to test your functions experimentally use difftime explained here 
-    http://www.cplusplus.com/reference/ctime/time/
-  */
-  return 0;
+	- to test your functions experimentally use difftime explained here
+	http://www.cplusplus.com/reference/ctime/time/
+	*/
+	return 0;
 }
 
 //read in flights from input file at fileName and store in a vector
-std::vector<Flight> readFlights(std::string fileName){
-  std::vector<Flight> flights;
-  std::ifstream inFile(fileName.c_str());
-  std::string dummy;  
-  getline(inFile, dummy);
-  while(!inFile.eof()) {
-    Flight temp;
-    std::string str;
-    getline(inFile, str, ',');
-    temp.flightNum = str;
-    getline(inFile, str, ',');
-    temp.destination = str;
-    getline(inFile, str, ',');
-    temp.departureTime = str;
-    getline(inFile, str, '\n');
-    temp.gateNum = str;
-    flights.push_back(temp);
-  }
-  return flights;
+vector<Flight> readFlights(string fileName) {
+	vector<Flight> flights;
+	ifstream inFile(fileName);
+	if (!inFile.is_open()) {
+		cout << "Error opening file." << endl;
+	}
+	else {
+		string dummy;
+		getline(inFile, dummy);
+		while (!inFile.eof()) {
+			Flight temp;
+			string str;
+			getline(inFile, str, ',');
+			temp.flightNum = str;
+			getline(inFile, str, ',');
+			temp.destination = str;
+			getline(inFile, str, ',');
+			temp.departureTime = str;
+			getline(inFile, str, '\n');
+			temp.gateNum = str;
+			flights.push_back(temp);
+		}
+	}
+	return flights;
 }
 
-void printFlights(std::vector<Flight> flights){
-  //ONLY PRINT TO TERMINAL FOR 10 OR LESS FLIGHTS
-  if (flights.size() < 12) {
-    printf("%-15s %-20s %-15s %-s\n", 
-      "Flight Number", 
-      "Destination", 
-      "Departure Time",
-      "Gate Number");
-    for(int i = 0; i < flights.size(); i++){
-      printf("%-15s %-20s %-15s %-s\n", 
-        flights.at(i).flightNum.c_str(), 
-        flights.at(i).destination.c_str(), 
-        flights.at(i).departureTime.c_str(),
-        flights.at(i).gateNum.c_str());
-    }
-  }
-  //Printing to file
-  FILE * outFile;
-  outFile = fopen("Sorted.txt", "w");
-  fprintf(outFile, "%-15s %-20s %-15s %-s\n", 
-    "Flight Number", 
-    "Destination", 
-    "Departure Time",
-    "Gate Number");
-  for(int i = 0; i < flights.size(); i++){
-    fprintf(outFile, "%-15s %-20s %-15s %-s\n", 
-      flights.at(i).flightNum.c_str(), 
-      flights.at(i).destination.c_str(), 
-      flights.at(i).departureTime.c_str(),
-      flights.at(i).gateNum.c_str());
-  }
-  fclose(outFile);
-  return;
+void printFlights(vector<Flight> flights) {
+	//ONLY PRINT TO TERMINAL FOR 10 OR LESS FLIGHTS
+	if (flights.size() < 12) {
+		printf("%-15s %-20s %-15s %-s\n",
+			"Flight Number",
+			"Destination",
+			"Departure Time",
+			"Gate Number");
+		for (int i = 0; i < flights.size(); i++) {
+			printf("%-15s %-20s %-15s %-s\n",
+				flights.at(i).flightNum.c_str(),
+				flights.at(i).destination.c_str(),
+				flights.at(i).departureTime.c_str(),
+				flights.at(i).gateNum.c_str());
+		}
+	}
+	//Printing to file
+	FILE * outFile;
+	outFile = fopen("Sorted.txt", "w");
+	fprintf(outFile, "%-15s %-20s %-15s %-s\n",
+		"Flight Number",
+		"Destination",
+		"Departure Time",
+		"Gate Number");
+	for (int i = 0; i < flights.size(); i++) {
+		fprintf(outFile, "%-15s %-20s %-15s %-s\n",
+			flights.at(i).flightNum.c_str(),
+			flights.at(i).destination.c_str(),
+			flights.at(i).departureTime.c_str(),
+			flights.at(i).gateNum.c_str());
+	}
+	fclose(outFile);
+	return;
 };
--- PA2-18a-TemplateCode/sort.cpp
+++ PA2-18a-TemplateCode/sort.cpp
@@ -6,78 +6,104 @@ sort.cpp
 */
 
 #include "sort.h"
-
+#include <iostream>
 
 //use this to keep track of comparisons
 int num_cmps;
-
 std::vector<Flight> selection_sort(std::vector<Flight> flights,
-				   SortOption sortOpt)
+	SortOption sortOpt)
 {
-	if (sortOpt == ByDepartureTime) {		for (int k = 0; k < flights.size() - 1; k++) {			int index = k;			Flight tracker = flights.at(k);			for (int i = k + 1; i < flights.size(); i++) {				if (compareToDepartureTime(flights.at(i), flights.at(index))) {					index = i;				}			}			if (compareToDepartureTime(tracker, flights.at(k))) {				flights.at(k) = flights.at(index);				flights.at(index) = tracker;			}		}	}
-
-	else if (sortOpt == ByDestination) {		for (int k = 0; k < flights.size() - 1; k++) {			int index = k;			Flight tracker = flights.at(k);			for (int i = k + 1; i < flights.size(); i++) {				if (compareToDestination(flights.at(i), flights.at(index))) {					index = i;				}			}			if (compareToDestination(tracker, flights.at(k))) {				flights.at(k) = flights.at(index);				flights.at(index) = tracker;			}		}	}
+	if (sortOpt == ByDepartureTime) {
+		for (int k = 0; k < flights.size() - 1; k++) {
+			int min = k;
+			Flight tracker = flights.at(k);
+			for (int i = k + 1; i < flights.size(); i++) {
+				if (!compareToDepartureTime(flights.at(i), flights.at(min))) {
+					min = i;
+				}
+			}
+			if (compareToDepartureTime(flights.at(k), flights.at(min))) {
+				flights.at(k) = flights.at(min);
+				flights.at(min) = tracker;
+			}
+		}
+	}
+	else if (sortOpt == ByDestination) {
+		for (int k = 0; k < flights.size() - 1; k++) {
+			int min = k;
+			Flight tracker = flights.at(k);
+			for (int i = k + 1; i < flights.size(); i++) {
+				if (!compareToDestination(flights.at(i), flights.at(min))) {
+					min = i;
+				}
+			}
+			if (compareToDestination(flights.at(k), flights.at(min))) {
+				flights.at(k) = flights.at(min);
+				flights.at(min) = tracker;
+			}
+		}
+	}
 	return flights;
 }
 
 std::vector<Flight> insertion_sort(std::vector<Flight> flights, SortOption sortOpt) {
 
-  if (sortOpt == ByDestination) {
-    for (int i = 0; i < flights.size(); i++) {
-        Flight tracker = flights.at(i);
-        int j = i-1;
-        while (j>= 0 && compareToDestination(flights.at(j), tracker)) {
-            flights.at(j+1) = flights.at(j);
-            j = j - 1;
-        }
-        flights.at(j+1) = tracker;
-    }
-  }
-  else if (sortOpt = ByDepartureTime) {
-    for (int i = 0; i < flights.size(); i++) {
-        Flight tracker = flights.at(i);
-        int j = i-1;
-        while (j>= 0 && compareToDepartureTime(flights.at(j), tracker)) {
-            flights.at(j+1) = flights.at(j);
-            j = j - 1;
-        }
-        flights.at(j+1) = tracker;
-    }
-  }
-  return flights;
+	if (sortOpt == ByDestination) {
+		for (int i = 0; i < flights.size(); i++) {
+			Flight tracker = flights.at(i);
+			int j = i - 1;
+			while (j >= 0 && compareToDestination(flights.at(j), tracker)) {
+				flights.at(j + 1) = flights.at(j);
+				j = j - 1;
+			}
+			flights.at(j + 1) = tracker;
+		}
+	}
+	else if (sortOpt = ByDepartureTime) {
+		for (int i = 0; i < flights.size(); i++) {
+			Flight tracker = flights.at(i);
+			int j = i - 1;
+			while (j >= 0 && compareToDepartureTime(flights.at(j), tracker)) {
+				flights.at(j + 1) = flights.at(j);
+				j = j - 1;
+			}
+			flights.at(j + 1) = tracker;
+		}
+	}
+	return flights;
 }
 
 std::vector<Flight> bubble_sort(std::vector<Flight> flights,
-				SortOption sortOpt)
+	SortOption sortOpt)
 {
-  int swaps = 1;
-  Flight temp;
-    if(sortOpt == ByDestination){
-      while(swaps != 0){
-        for(int j = 0; j < flights.size();j++){ //inner loop, don't do i+1 to go out of bounds
-          swaps = 0;
-          if(compareToDestination(flights.at(j),flights.at(j+1))){ //swap
-            swaps++;
-            temp = flights.at(j);
-            flights.at(j) = flights.at(j+1);
-            flights.at(j+1) = temp;
-          }
-        } //end inner loop
-      } //end outer loop
-    }
+	int swaps = 1;
+	Flight temp;
+	if (sortOpt == ByDestination) {
+		while (swaps != 0) {
+			for (int j = 0; j < flights.size(); j++) { //inner loop, don't do i+1 to go out of bounds
+				swaps = 0;
+				if (compareToDestination(flights.at(j), flights.at(j + 1))) { //swap
+					swaps++;
+					temp = flights.at(j);
+					flights.at(j) = flights.at(j + 1);
+					flights.at(j + 1) = temp;
+				}
+			} //end inner loop
+		} //end outer loop
+	}
 
-    if(sortOpt == ByDepartureTime){
-      while(swaps != 0){
-        for(int j = 0; j < flights.size();j++){ //inner loop, don't do i+1 to go out of bounds
-          swaps = 0;
-          if(compareToDepartureTime(flights.at(j),flights.at(j+1))){ //swap
-            swaps++;
-            temp = flights.at(j);
-            flights.at(j) = flights.at(j+1);
-            flights.at(j+1) = temp;
-          }
-        } //end inner loop
-      } //end outer loop
-    }
-    return flights;
+	if (sortOpt == ByDepartureTime) {
+		while (swaps != 0) {
+			for (int j = 0; j < flights.size(); j++) { //inner loop, don't do i+1 to go out of bounds
+				swaps = 0;
+				if (compareToDepartureTime(flights.at(j), flights.at(j + 1))) { //swap
+					swaps++;
+					temp = flights.at(j);
+					flights.at(j) = flights.at(j + 1);
+					flights.at(j + 1) = temp;
+				}
+			} //end inner loop
+		} //end outer loop
+	}
+	return flights;
 }
